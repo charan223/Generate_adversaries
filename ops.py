695893f4122b4b7240af2878a993fd897295ea7a
charan223 <ragireddycharan223@gmail.com>
2018-04-13 04:07:20 +0530

    First commit

---
+++ ops.py
@@ -0,0 +1,636 @@
+import numpy as np
+import tensorflow as tf
+
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
+
+_params = {}
+_param_aliases = {}
+
+
+
+
+_default_weightnorm = False
+def enable_default_weightnorm():
+    global _default_weightnorm
+    _default_weightnorm = True
+
+def disable_default_weightnorm():
+    global _default_weightnorm
+    _default_weightnorm = False
+
+_weights_stdev = None
+def set_weights_stdev(weights_stdev):
+    global _weights_stdev
+    _weights_stdev = weights_stdev
+
+def unset_weights_stdev():
+    global _weights_stdev
+    _weights_stdev = None
+
+
+
+import os
+import urllib
+import gzip
+import cPickle as pickle
+
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import collections
+import time
+
+import scipy.misc
+from scipy.misc import imsave
+
+
+
+
+
+
+
+
+
+def Conv2D(name, input_dim, output_dim, filter_size, inputs, he_init=True, mask_type=None, stride=1, weightnorm=None, biases=True, gain=1.):
+    """
+    inputs: tensor of shape (batch size, num channels, height, width)
+    mask_type: one of None, 'a', 'b'
+
+    returns: tensor of shape (batch size, num channels, height, width)
+    """
+    with tf.name_scope(name) as scope:
+
+        if mask_type is not None:
+            mask_type, mask_n_channels = mask_type
+
+            mask = np.ones(
+                (filter_size, filter_size, input_dim, output_dim), 
+                dtype='float32'
+            )
+            center = filter_size // 2
+
+            # Mask out future locations
+            # filter shape is (height, width, input channels, output channels)
+            mask[center+1:, :, :, :] = 0.
+            mask[center, center+1:, :, :] = 0.
+
+            # Mask out future channels
+            for i in xrange(mask_n_channels):
+                for j in xrange(mask_n_channels):
+                    if (mask_type=='a' and i >= j) or (mask_type=='b' and i > j):
+                        mask[
+                            center,
+                            center,
+                            i::mask_n_channels,
+                            j::mask_n_channels
+                        ] = 0.
+
+
+        def uniform(stdev, size):
+            return np.random.uniform(
+                low=-stdev * np.sqrt(3),
+                high=stdev * np.sqrt(3),
+                size=size
+            ).astype('float32')
+
+        fan_in = input_dim * filter_size**2
+        fan_out = output_dim * filter_size**2 / (stride**2)
+
+        if mask_type is not None: # only approximately correct
+            fan_in /= 2.
+            fan_out /= 2.
+
+        if he_init:
+            filters_stdev = np.sqrt(4./(fan_in+fan_out))
+        else: # Normalized init (Glorot & Bengio)
+            filters_stdev = np.sqrt(2./(fan_in+fan_out))
+
+        if _weights_stdev is not None:
+            filter_values = uniform(
+                _weights_stdev,
+                (filter_size, filter_size, input_dim, output_dim)
+            )
+        else:
+            filter_values = uniform(
+                filters_stdev,
+                (filter_size, filter_size, input_dim, output_dim)
+            )
+
+        # print "WARNING IGNORING GAIN"
+        filter_values *= gain
+
+        filters = param(name+'.Filters', filter_values)
+
+        if weightnorm==None:
+            weightnorm = _default_weightnorm
+        if weightnorm:
+            norm_values = np.sqrt(np.sum(np.square(filter_values), axis=(0,1,2)))
+            target_norms = param(
+                name + '.g',
+                norm_values
+            )
+            with tf.name_scope('weightnorm') as scope:
+                norms = tf.sqrt(tf.reduce_sum(tf.square(filters), reduction_indices=[0,1,2]))
+                filters = filters * (target_norms / norms)
+
+        if mask_type is not None:
+            with tf.name_scope('filter_mask'):
+                filters = filters * mask
+
+        result = tf.nn.conv2d(
+            input=inputs, 
+            filter=filters, 
+            strides=[1, 1, stride, stride],
+            padding='SAME',
+            data_format='NCHW'
+        )
+
+        if biases:
+            _biases = param(
+                name+'.Biases',
+                np.zeros(output_dim, dtype='float32')
+            )
+
+            result = tf.nn.bias_add(result, _biases, data_format='NCHW')
+
+
+        return result
+
+
+
+
+#deconvolution function
+
+
+def Deconv2D(
+    name, 
+    input_dim, 
+    output_dim, 
+    filter_size, 
+    inputs, 
+    he_init=True,
+    weightnorm=None,
+    biases=True,
+    gain=1.,
+    mask_type=None,
+    ):
+    """
+    inputs: tensor of shape (batch size, height, width, input_dim)
+    returns: tensor of shape (batch size, 2*height, 2*width, output_dim)
+    """
+    with tf.name_scope(name) as scope:
+
+        if mask_type != None:
+            raise Exception('Unsupported configuration')
+
+        def uniform(stdev, size):
+            return np.random.uniform(
+                low=-stdev * np.sqrt(3),
+                high=stdev * np.sqrt(3),
+                size=size
+            ).astype('float32')
+
+        stride = 2
+        fan_in = input_dim * filter_size**2 / (stride**2)
+        fan_out = output_dim * filter_size**2
+
+        if he_init:
+            filters_stdev = np.sqrt(4./(fan_in+fan_out))
+        else: # Normalized init (Glorot & Bengio)
+            filters_stdev = np.sqrt(2./(fan_in+fan_out))
+
+
+        if _weights_stdev is not None:
+            filter_values = uniform(
+                _weights_stdev,
+                (filter_size, filter_size, output_dim, input_dim)
+            )
+        else:
+            filter_values = uniform(
+                filters_stdev,
+                (filter_size, filter_size, output_dim, input_dim)
+            )
+
+        filter_values *= gain
+
+        filters = param(
+            name+'.Filters',
+            filter_values
+        )
+
+        if weightnorm==None:
+            weightnorm = _default_weightnorm
+        if weightnorm:
+            norm_values = np.sqrt(np.sum(np.square(filter_values), axis=(0,1,3)))
+            target_norms = param(
+                name + '.g',
+                norm_values
+            )
+            with tf.name_scope('weightnorm') as scope:
+                norms = tf.sqrt(tf.reduce_sum(tf.square(filters), reduction_indices=[0,1,3]))
+                filters = filters * tf.expand_dims(target_norms / norms, 1)
+
+
+        inputs = tf.transpose(inputs, [0,2,3,1], name='NCHW_to_NHWC')
+
+        input_shape = tf.shape(inputs)
+        try: # tf pre-1.0 (top) vs 1.0 (bottom)
+            output_shape = tf.pack([input_shape[0], 2*input_shape[1], 2*input_shape[2], output_dim])
+        except Exception as e:
+            output_shape = tf.stack([input_shape[0], 2*input_shape[1], 2*input_shape[2], output_dim])
+
+        result = tf.nn.conv2d_transpose(
+            value=inputs, 
+            filter=filters,
+            output_shape=output_shape, 
+            strides=[1, 2, 2, 1],
+            padding='SAME'
+        )
+
+        if biases:
+            _biases = param(
+                name+'.Biases',
+                np.zeros(output_dim, dtype='float32')
+            )
+            result = tf.nn.bias_add(result, _biases)
+
+        result = tf.transpose(result, [0,3,1,2], name='NHWC_to_NCHW')
+
+
+        return result
+
+
+#Linear function
+
+def Linear(
+        name, 
+        input_dim, 
+        output_dim, 
+        inputs,
+        biases=True,
+        initialization=None,
+        weightnorm=None,
+        gain=1.
+        ):
+    """
+    initialization: None, `lecun`, 'glorot', `he`, 'glorot_he', `orthogonal`, `("uniform", range)`
+    """
+    with tf.name_scope(name) as scope:
+
+        def uniform(stdev, size):
+            if _weights_stdev is not None:
+                stdev = _weights_stdev
+            return np.random.uniform(
+                low=-stdev * np.sqrt(3),
+                high=stdev * np.sqrt(3),
+                size=size
+            ).astype('float32')
+
+        if initialization == 'lecun':# and input_dim != output_dim):
+            # disabling orth. init for now because it's too slow
+            weight_values = uniform(
+                np.sqrt(1./input_dim),
+                (input_dim, output_dim)
+            )
+
+        elif initialization == 'glorot' or (initialization == None):
+
+            weight_values = uniform(
+                np.sqrt(2./(input_dim+output_dim)),
+                (input_dim, output_dim)
+            )
+
+        elif initialization == 'he':
+
+            weight_values = uniform(
+                np.sqrt(2./input_dim),
+                (input_dim, output_dim)
+            )
+
+        elif initialization == 'glorot_he':
+
+            weight_values = uniform(
+                np.sqrt(4./(input_dim+output_dim)),
+                (input_dim, output_dim)
+            )
+
+        elif initialization == 'orthogonal' or \
+            (initialization == None and input_dim == output_dim):
+            
+            # From lasagne
+            def sample(shape):
+                if len(shape) < 2:
+                    raise RuntimeError("Only shapes of length 2 or more are "
+                                       "supported.")
+                flat_shape = (shape[0], np.prod(shape[1:]))
+                 # TODO: why normal and not uniform?
+                a = np.random.normal(0.0, 1.0, flat_shape)
+                u, _, v = np.linalg.svd(a, full_matrices=False)
+                # pick the one with the correct shape
+                q = u if u.shape == flat_shape else v
+                q = q.reshape(shape)
+                return q.astype('float32')
+            weight_values = sample((input_dim, output_dim))
+        
+        elif initialization[0] == 'uniform':
+        
+            weight_values = np.random.uniform(
+                low=-initialization[1],
+                high=initialization[1],
+                size=(input_dim, output_dim)
+            ).astype('float32')
+
+        else:
+
+            raise Exception('Invalid initialization!')
+
+        weight_values *= gain
+
+        weight = param(
+            name + '.W',
+            weight_values
+        )
+
+        if weightnorm==None:
+            weightnorm = _default_weightnorm
+        if weightnorm:
+            norm_values = np.sqrt(np.sum(np.square(weight_values), axis=0))
+            # norm_values = np.linalg.norm(weight_values, axis=0)
+
+            target_norms = param(
+                name + '.g',
+                norm_values
+            )
+
+            with tf.name_scope('weightnorm') as scope:
+                norms = tf.sqrt(tf.reduce_sum(tf.square(weight), reduction_indices=[0]))
+                weight = weight * (target_norms / norms)
+
+        # if 'Discriminator' in name:
+        #     print "WARNING weight constraint on {}".format(name)
+        #     weight = tf.nn.softsign(10.*weight)*.1
+
+        if inputs.get_shape().ndims == 2:
+            result = tf.matmul(inputs, weight)
+        else:
+            reshaped_inputs = tf.reshape(inputs, [-1, input_dim])
+            result = tf.matmul(reshaped_inputs, weight)
+            result = tf.reshape(result, tf.pack(tf.unpack(tf.shape(inputs))[:-1] + [output_dim]))
+
+        if biases:
+            result = tf.nn.bias_add(
+                result,
+                param(
+                    name + '.b',
+                    np.zeros((output_dim,), dtype='float32')
+                )
+            )
+
+        return result
+
+#####################################################################
+
+def param(name, *args, **kwargs):
+    """
+    A wrapper for `tf.Variable` which enables parameter sharing in models.
+    
+    Creates and returns theano shared variables similarly to `tf.Variable`, 
+    except if you try to create a param with the same name as a 
+    previously-created one, `param(...)` will just return the old one instead of 
+    making a new one.
+
+    This constructor also adds a `param` attribute to the shared variables it 
+    creates, so that you can easily search a graph for all params.
+    """
+
+    if name not in _params:
+        kwargs['name'] = name
+        param = tf.Variable(*args, **kwargs)
+        param.param = True
+        _params[name] = param
+    result = _params[name]
+    i = 0
+    while result in _param_aliases:
+        # print 'following alias {}: {} to {}'.format(i, result, _param_aliases[result])
+        i += 1
+        result = _param_aliases[result]
+    return result
+
+def params_with_name(name):
+    return [p for n,p in _params.items() if name in n]
+
+def delete_all_params():
+    _params.clear()
+
+def alias_params(replace_dict):
+    for old,new in replace_dict.items():
+        # print "aliasing {} to {}".format(old,new)
+        _param_aliases[old] = new
+
+def delete_param_aliases():
+    _param_aliases.clear()
+
+
+def print_model_settings(locals_):
+    print "Uppercase local vars:"
+    all_vars = [(k,v) for (k,v) in locals_.items() if (k.isupper() and k!='T' and k!='SETTINGS' and k!='ALL_SETTINGS')]
+    all_vars = sorted(all_vars, key=lambda x: x[0])
+    for var_name, var_value in all_vars:
+        print "\t{}: {}".format(var_name, var_value)
+
+
+def print_model_settings_dict(settings):
+    print "Settings dict:"
+    all_vars = [(k,v) for (k,v) in settings.items()]
+    all_vars = sorted(all_vars, key=lambda x: x[0])
+    for var_name, var_value in all_vars:
+        print "\t{}: {}".format(var_name, var_value)
+
+
+####################################################################
+
+
+
+def param(name, *args, **kwargs):
+    """
+    A wrapper for `tf.Variable` which enables parameter sharing in models.
+    
+    Creates and returns theano shared variables similarly to `tf.Variable`, 
+    except if you try to create a param with the same name as a 
+    previously-created one, `param(...)` will just return the old one instead of 
+    making a new one.
+
+    This constructor also adds a `param` attribute to the shared variables it 
+    creates, so that you can easily search a graph for all params.
+    """
+
+    if name not in _params:
+        kwargs['name'] = name
+        param = tf.Variable(*args, **kwargs)
+        param.param = True
+        _params[name] = param
+    result = _params[name]
+    i = 0
+    while result in _param_aliases:
+        # print 'following alias {}: {} to {}'.format(i, result, _param_aliases[result])
+        i += 1
+        result = _param_aliases[result]
+    return result
+
+def params_with_name(name):
+    return [p for n,p in _params.items() if name in n]
+
+
+
+
+###################
+
+
+def mnist_generator(data, batch_size, n_labelled, limit=None):
+    images, targets = data
+
+    rng_state = np.random.get_state()
+    np.random.shuffle(images)
+    np.random.set_state(rng_state)
+    np.random.shuffle(targets)
+    if limit is not None:
+        print "WARNING ONLY FIRST {} MNIST DIGITS".format(limit)
+        images = images.astype('float32')[:limit]
+        targets = targets.astype('int32')[:limit]
+    if n_labelled is not None:
+        labelled = np.zeros(len(images), dtype='int32')
+        labelled[:n_labelled] = 1
+
+    def get_epoch():
+        rng_state = np.random.get_state()
+        np.random.shuffle(images)
+        np.random.set_state(rng_state)
+        np.random.shuffle(targets)
+
+        if n_labelled is not None:
+            np.random.set_state(rng_state)
+            np.random.shuffle(labelled)
+
+        image_batches = images.reshape(-1, batch_size, 784)
+        target_batches = targets.reshape(-1, batch_size)
+
+        if n_labelled is not None:
+            labelled_batches = labelled.reshape(-1, batch_size)
+
+            for i in xrange(len(image_batches)):
+                yield (np.copy(image_batches[i]), np.copy(target_batches[i]), np.copy(labelled))
+
+        else:
+
+            for i in xrange(len(image_batches)):
+                yield (np.copy(image_batches[i]), np.copy(target_batches[i]))
+
+    return get_epoch
+
+def load(batch_size, test_batch_size, n_labelled=None):
+    filepath = '/tmp/mnist.pkl.gz'
+    url = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+
+    if not os.path.isfile(filepath):
+        print "Couldn't find MNIST dataset in /tmp, downloading..."
+        urllib.urlretrieve(url, filepath)
+
+    with gzip.open('/tmp/mnist.pkl.gz', 'rb') as f:
+        train_data, dev_data, test_data = pickle.load(f)
+
+    return (
+        mnist_generator(train_data, batch_size, n_labelled), 
+        mnist_generator(dev_data, test_batch_size, n_labelled), 
+        mnist_generator(test_data, test_batch_size, n_labelled)
+    )
+
+def load_data():
+    filepath = '/tmp/mnist.pkl.gz'
+    url = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+
+    if not os.path.isfile(filepath):
+        print "Couldn't find MNIST dataset in /tmp, downloading..."
+        urllib.urlretrieve(url, filepath)
+
+    with gzip.open('/tmp/mnist.pkl.gz', 'rb') as f:
+        train_data, dev_data, test_data = pickle.load(f)
+
+    return train_data, dev_data, test_data
+###############
+
+
+_since_beginning = collections.defaultdict(lambda: {})
+_since_last_flush = collections.defaultdict(lambda: {})
+
+_iter = [0]
+def set(start=0):
+    _iter[0] = start
+
+def tick():
+    _iter[0] += 1
+
+def plot(name, value):
+    _since_last_flush[name][_iter[0]] = value
+
+
+#########
+
+
+
+def save_images(X, save_path):
+    # [0, 1] -> [0,255]
+    if isinstance(X.flatten()[0], np.floating):
+        X = (255.99*X).astype('uint8')
+
+    n_samples = X.shape[0]
+    rows = int(np.sqrt(n_samples))
+    while n_samples % rows != 0:
+        rows -= 1
+
+    nh, nw = rows, n_samples/rows
+
+    if X.ndim == 2:
+        X = np.reshape(X, (X.shape[0], int(np.sqrt(X.shape[1])), int(np.sqrt(X.shape[1]))))
+
+    if X.ndim == 4:
+        # BCHW -> BHWC
+        X = X.transpose(0,2,3,1)
+        h, w = X[0].shape[:2]
+        img = np.zeros((h*nh, w*nw, 3))
+    elif X.ndim == 3:
+        h, w = X[0].shape[:2]
+        img = np.zeros((h*nh, w*nw))
+
+    for n, x in enumerate(X):
+        j = n/nw
+        i = n%nw
+        img[j*h:j*h+h, i*w:i*w+w] = x
+
+    imsave(save_path, img)
+
+###########
+def flush(output_dir=''):
+    prints = []
+
+    for name, vals in _since_last_flush.items():
+        prints.append("{}\t{}".format(name, np.mean(vals.values())))
+        _since_beginning[name].update(vals)
+
+        x_vals = np.sort(_since_beginning[name].keys())
+        y_vals = [_since_beginning[name][x] for x in x_vals]
+
+        plt.clf()
+        plt.plot(x_vals, y_vals)
+        plt.xlabel('iteration')
+        plt.ylabel(name)
+        plt.savefig(os.path.join(output_dir, name.replace(' ', '_')+'.png'))
+
+    print "iter {}\t{}".format(_iter[0], "\t".join(prints))
+    _since_last_flush.clear()
+
+    with open(os.path.join(output_dir, 'log.pkl'), 'wb') as f:
+        pickle.dump(dict(_since_beginning), f, pickle.HIGHEST_PROTOCOL)
